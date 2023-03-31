331b1dde29bbfee939cb7dd407132b7f2d9ef05e
Arindam Chakraborty <arismart99@gmail.com>
2023-03-31 14:42:34 +0530

    Add static documentation for view & manager methods for instruments

--- algo_trading/models/managers/instrument_manager.py
+++ algo_trading/models/managers/instrument_manager.py
@@ -12,27 +12,70 @@ class InstrumentManager(models.Manager):
     KEY_FOR_FIRST_OPEN_DATE_IN_INFO = 'firstTradeDateMilliseconds'
     KEY_FOR_INSTRUMENT_TYPE_IN_INFO = 'typeDisp'
 
-    def process_new_instrument(self, new_symbol):
-        """ Grab OHLCV data for the given symbol for all time
-            Create an Instrument Record for the symbol
-            Create OHLCV records for all obtained data
-            Raise error if API fails or incorrect symbol is used
+    def process_new_instrument(self, new_symbol: str):
+        """Process a new symbol to be recorded in the model `Instrument`
+
+        Verify if the symbol is a valid one, if not raise an error
+        Gather `info` for the symbol from the yfinance API
+        Ingest that info into a new record
+        Raise error if API fails
+
+        Parameters
+        ----------
+        new_symbol: str
+            The symbol of the instrument that needs to be processed
+
+        Returns
+        -------
+        The new `Instrument` instance that is created for the symbol.
+
+        Raises
+        ------
+        InvalidTickerSymbolError
+            If the symbol was invalid and no data can be found in yfinance
+        AttributeError
+            If there is any unreferred attribute being accessed. This is in addition to the symbol being
+            invalid, which is also identified by an AttributeError from the API response
+        API error
+            Any errors coming from the API, passed on as-is to the caller.
+            Any errors coming from the record creation, is also passed on as-is.
         """
+
         try:
             ticker = yf.Ticker(new_symbol)
             instrument_info = ticker.info
 
             return self.ingest_new_instrument(instrument_info)
         except AttributeError as error:
+            # If the symbol was invalid, ticker.info raises an AttributeError with
+            # NoneType in the error message
             if 'NoneType' in error.args[0]:
                 raise InvalidTickerSymbolError
             else:
+                # If there were any other AttributeError's, we pass them on to the caller of this method
                 raise error
         except Exception as exc:
+            # If any other error from the API occurs, we pass them on to the caller of this method
             raise exc
 
-    def ingest_new_instrument(self, info):
-        """ Grab info and create an instrument out of that"""
+    def ingest_new_instrument(self, info: dict):
+        """From info create an `Instrument` record
+
+        Pull pre-defined keys from the `info` dict to create values for the record field
+        Convert date from epoch time in ms to python date format
+        Convert type from the string +Equity+ to its matching label in choices +EQ+
+
+        Parameters
+        ----------
+        info: dict
+            A dictionary containing all the info about the stock as obtained from yfinance API.
+            Sample file is at `collectibles/yf_ticker_info_for_symbol.json
+
+        Returns
+        -------
+        A record of type `Instrument` as created from the data passed in `info`
+        """
+
         first_open_date_from_info = info[self.KEY_FOR_FIRST_OPEN_DATE_IN_INFO] / 1000
         first_open_date = datetime.datetime.fromtimestamp(first_open_date_from_info)
 
@@ -46,5 +89,22 @@ class InstrumentManager(models.Manager):
             instrument_type=instrument_type,
         )
 
-    def download_historical_data(self, instruments):
-        """Download historical data for all the instruments passed"""
+    def download_historical_data(self, instrument):
+        """Downloads historical data & creates DB entries for the instrument passed.
+
+        For a given instrument downloads its historical data till date.
+        Stores each day's entry as individual records of `Ohlcv_data`.
+        Data downloaded is only for interval=1d, as of now.
+        Any rows with any missing information is dropped using `pandas#dropna`
+        Not to be used to update an instrument's OHLCV data.
+        To be used only for one-time seeding of past data till date.
+
+        Parameters
+        ----------
+        instrument: Instrument
+            The Instrument instance for which historical data is required.
+
+        """
+
+        # TODO: allow range/interval to be passed as an arg to indicate the frequency at which
+        # data is desired. Currently one 1d interval is supported.
--- algo_trading/views/instruments_view.py
+++ algo_trading/views/instruments_view.py
@@ -9,6 +9,8 @@ from ..exceptions import InvalidTickerSymbolError
 
 @require_http_methods(["GET"])
 def instrument_by_pk(request, pk: int):
+    """Show an `Instrument` instance identified by its primary key: `pk`"""
+
     instrument = get_object_or_404(Instrument, pk=pk)
     return display_instrument(
         request,
@@ -18,6 +20,8 @@ def instrument_by_pk(request, pk: int):
 
 @require_http_methods(["GET"])
 def instrument_by_symbol(request, symbol: str):
+    """Show an `Instrument` instance identified by its `symbol`"""
+
     instrument = get_object_or_404(Instrument, symbol=symbol)
     return display_instrument(
         request,
@@ -26,6 +30,8 @@ def instrument_by_symbol(request, symbol: str):
 
 
 def display_instrument(request, instrument: Instrument):
+    """Common function to show an `Instrument`"""
+
     return render(
         request,
         'instruments/show.html',
@@ -37,6 +43,19 @@ def display_instrument(request, instrument: Instrument):
 
 @require_http_methods(["GET", "POST"])
 def add_instruments(request):
+    """Add an instrument into the system
+
+    Uses a form modeled on the `Instrument` class and uses only the field `symbol`
+    The form is validated against the model and at the HTTP level
+    If the request was a GET, it means user navigated to this page, so we show an empty form.
+    The empty form prompts the user to enter the symbol that needs to be added to our system
+    If the request was a POST, it means user typed in the symbol and submitted the form.
+    A new record is attempted to be created from the symbol.
+    If the symbol is correct, and no API failure happens, the symbol is added and navigation
+    directed to that Instrument's show page.
+    If the symbol is incorrect or an API failure happens, relevant error message is shown on the form.
+    """
+
     if request.method == 'POST':
         form = AddInstrumentForm(request.POST)
 
@@ -50,7 +69,32 @@ def add_instruments(request):
     return render(request, 'instruments/add_instrument.html', {'form': form})
 
 
-def attempt_add_new_instrument(form):
+def attempt_add_new_instrument(form: AddInstrumentForm) -> Instrument:
+    """Tries to create a new instrument from the form data.
+
+    Internal function for the view, not to be called from outside.
+    Processes the symbol through the yfinance API to collect basic info about the stock
+    and creates a new Instrument instance from that info
+    For the newly created Instrument, downloads all historical data till date and creates
+    a record of type _FILLME_ for every available date in the historical data with their
+    OHLCV entries.
+
+    Parameters
+    ----------
+    form: AddInstrumentForm
+        An instance of AddInstrumentForm bound with the POST data from the request
+
+    Returns
+    ----------
+    new_instrument: Instrument
+        The new `Instrument` instance that was created with the form data, a la, the symbol
+
+    Raises
+    ------
+    Adds errors to the form on the field `symbol` if it was invalid,
+    or to the general non-form-field if there were any other API/model level errors
+    """
+
     try:
         new_instrument = Instrument.objects.process_new_instrument(form.cleaned_data['symbol'])
         Instrument.objects.download_historical_data([new_instrument])

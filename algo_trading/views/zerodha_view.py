dd562e16ad50e7127d03ef22feffba0ffb8c44ed
Arindam Chakraborty <arismart99@gmail.com>
2023-05-25 19:51:57 +0530

    move kiteconnect to a singleton instance

--- algo_trading/views/zerodha_view.py
+++ algo_trading/views/zerodha_view.py
@@ -4,7 +4,7 @@ from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import JsonResponse
 
-from kiteconnect import KiteConnect
+from ..models.kite_broker import KiteBroker
 
 
 ZERODHA_API_KEY = "g54suy4ucztqh9se"
@@ -13,7 +13,7 @@ ZERODHA_API_SECRET = "i637gje20mtuxco4kczs8uc666ogpsop"
 
 @require_http_methods(["GET"])
 def zerodha_login_url(request):
-    kite = KiteConnect(api_key=ZERODHA_API_KEY)
+    kite = KiteBroker(api_key=ZERODHA_API_KEY)
     redirect_string = "&redirect_params=" + urlencode({'redirect_to': request.GET.get('next')})
     return JsonResponse({'zerodha_login_url': kite.login_url() + redirect_string})
 
@@ -23,7 +23,7 @@ def zerodha_login_url(request):
 def zerodha_req_token(request):
     zerodha_request_token = request.GET.get('request_token')
     # todo add handler if token is nil or status/success is false
-    kite = KiteConnect(api_key=ZERODHA_API_KEY)
+    kite = KiteBroker(api_key=ZERODHA_API_KEY)
 
     data = kite.generate_session(zerodha_request_token, api_secret=ZERODHA_API_SECRET)
     kite.set_access_token(data["access_token"])

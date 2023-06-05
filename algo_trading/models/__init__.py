51d46e8e5cf50bb2a67b1f73761265f6d9f067be
Arindam Chakraborty <arismart99@gmail.com>
2023-06-05 14:54:39 +0530

    migrate order history model

--- algo_trading/models/__init__.py
+++ algo_trading/models/__init__.py
@@ -1,5 +1,5 @@
 from .instruments import Instrument
-from .ohlc_data_daily.py import OhlcDataDaily
+from .ohlc_data_daily import OhlcDataDaily
 from .order import Order
 from .order_history import OrderHistory
 from .trading_signal import TradingSignal

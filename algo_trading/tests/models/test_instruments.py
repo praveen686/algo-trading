40fb9298ef077565b848cfa4e4fd8a00e357fb76
Arindam Chakraborty <arismart99@gmail.com>
2023-06-12 11:14:46 +0530

    add skeleton test

--- algo_trading/tests/models/test_instruments.py
+++ algo_trading/tests/models/test_instruments.py
@@ -4,9 +4,8 @@ from algo_trading.models import Instrument
 
 
 class InstrumentsTest(TestCase):
-    def setUpTestData(cls):
-        # Set up non-modified objects used by all test methods
-        Instrument.objects.create(symbol="ABC", name="Test Company")
+    def setUpTestData():
+        Instrument.objects.create(trading_symbol="ABC", name="Test Company")
 
     def test_instrument_token_label(self):
         ins = Instrument.objects.get(id=1)
@@ -14,6 +13,5 @@ class InstrumentsTest(TestCase):
         self.assertEqual(field_label, "zerodha instrument token")
 
     def test_get_absolute_url(self):
-        author = Instrument.objects.get(id=1)
-        # This will also fail if the urlconf is not defined.
-        self.assertEqual(author.get_absolute_url(), "instruments/show/ABC")
+        ins = Instrument.objects.get(id=1)
+        self.assertEqual(ins.get_absolute_url(), "/instruments/show/ABC")
